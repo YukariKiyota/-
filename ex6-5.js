41acf9b2461e0c7bca59cb7ac4b77657254799a6
YukariKiyota <zm18053@josai.ac.jp>
2020-11-10 01:37:12 +0000

    message

--- ex6-5.js
+++ ex6-5.js
@@ -1,3 +1,3 @@
-var who = 'world';function sayhello() 
-{alert('Hello, ' + who + '!');}function taro() 
+var who = 'world';function sayhello65() 
+{alert('Hello, ' + who65 + '!');}function taro() 
 {who = '太郎';}function hanako() {who = '花子';}
