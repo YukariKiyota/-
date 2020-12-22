684bcf4090845421c86642828d897ef12fc604f6
YukariKiyota <zm18053@josai.ac.jp>
2020-12-22 01:49:51 +0000

    1222

--- ex06-9.js
+++ ex06-9.js
@@ -1,14 +1,5 @@
-function txtchange1() 
-{document.getElementById('txt1').innerHTML= 'ナンバーワン！';}
+function txtchange1() {  
+    document.getElementById('txt1').innerHTML= 'ナンバーワン！';}
 
 
 
-<div id="image1" >
-　<input type="button" value="画像の変更" onclick="imgchange1();">
-　<imgsrc='./image/josai.png' id="logo" alt="JosaiLogo"></div>
-
-<div id="image1" >
-　<input type="button" value="画像の変更" onclick="imgchange1();">
-　<imgsrc='./image/'div id="image1" >
-　<input type="button" value="画像の変更" onclick="imgchange1();">
-　<imgsrc='./image/img_pr191021_1.jpg' id="logo" alt="duffy">
